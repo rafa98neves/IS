389e13594442dea371e4fd8c01e1fed4b26d2503
rafael neves <rafaelalexandrerodriguesneves@gmail.com>
2019-11-01 16:44:55 +0000

    log in on doing

--- projeto2-parent/projeto2-web/src/main/java/servlet/RequestLogin.java
+++ projeto2-parent/projeto2-web/src/main/java/servlet/RequestLogin.java
@@ -1,5 +1,12 @@
 package servlet;
 
+import data.User;
+import ejb.LoginBean;
+import ejb.LoginBeanLocal;
+
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,14 +18,36 @@ import java.io.PrintWriter;
 @WebServlet("/RequestLogin")
 public class RequestLogin extends HttpServlet {
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  IOException {
-        response.setContentType("text/html");
-        try (PrintWriter out = response.getWriter()){
-            String name= request.getParameter("email");
-            String psw= request.getParameter("psw");
-        }finally {
+    @EJB LoginBeanLocal myLoginBean;
 
+    private void processRequest(HttpServletRequest request, HttpServletResponse response) throws  IOException {
+        response.setContentType("text/html");
+        ServletContext context= getServletContext();
+
+        try (PrintWriter out = response.getWriter()){
+            String email= request.getParameter("email");
+            String psw= request.getParameter("psw");
+
+            User user = myLoginBean.login(email,psw);
+            if(user != null){
+                RequestDispatcher rd = context.getRequestDispatcher("/registo.html");
+                rd.include(request, response);
+            }
+            else{
+                RequestDispatcher rd = context.getRequestDispatcher("/login.html");
+                rd.include(request, response);
+            }
+        } catch (Exception e){
+            //....
         }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  IOException {
+        processRequest(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws  IOException {
+        processRequest(request, response);
     }
 
 
