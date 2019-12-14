f4196eb913646c27973149e6ce75f3e90856b6ac
jmbds <jmbds@student.dei.uc.pt>
2019-12-14 20:09:56 +0000

    added revenue per item

--- project3/src/main/java/project3/kafka/Kafka_Streams.java
+++ project3/src/main/java/project3/kafka/Kafka_Streams.java
@@ -11,6 +11,7 @@ import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.Serializer;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.*;
@@ -42,7 +43,8 @@ public class Kafka_Streams {
         java.util.Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Integer().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Float().getClass());
 
         StreamsBuilder sales_builder = new StreamsBuilder();
         KStream<String, Sale> sales_stream = sales_builder.stream(salesT, Consumed.with(Serdes.String(), saleSerde));
@@ -53,10 +55,14 @@ public class Kafka_Streams {
 
         // Revenue per item
 
-        KTable<String, Long> outstream = sales_stream.mapValues(v -> v.getPrice()).groupByKey().count();
-        outstream.mapValues((k, v) -> k + " revenue = " + v).toStream().to(outtopicname, Produced.with(Serdes.String(), Serdes.String()));
+        KStream<Integer, Float> revenueStream = sales_stream.map((key, value) -> KeyValue.pair(value.getItem().getItem_id(), value.getPrice()));
+        KTable<Integer, Float> revenueTable = revenueStream.groupByKey().reduce((a,b) -> a + b);
+        revenueTable.mapValues((k, v) -> k + "=>" + v).toStream().to(outtopicname, Produced.with(Serdes.Integer(), Serdes.String()));
 
         // Expenses per item
+
+        //KStream<Integer, Float> expensesStream = purchases_stream.map((key, value) -> KeyValue.pair(value.getItem().getItem_id(), value.getPrice()));
+
         // Profit per item
         // Total revenues
         // Total expenses
